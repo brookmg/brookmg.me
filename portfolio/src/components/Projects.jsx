3e32bbba0b3ab54c9c26073c340a4001f0804ac7
brookmezgu <brookmezgebu64@gmail.com>
2019-11-16 13:46:27 +0300

    progress: corrected project description and image

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import ProjectItem from './ProjectItem.jsx'
-import Kuwas from '../img/Kuwas.jpg'
+import Kuwas from '../img/Kuwas.png'
+import Azemari from '../img/Azemari.png'
+import Hulu from '../img/Hulu.png'
+import Flick from '../img/Flick.png'
 
 class Projects extends Component {
 
@@ -21,26 +24,27 @@ class Projects extends Component {
                             />
 
                             <ProjectItem
+                                projectName="Azemari"
+                                projectDescription="A music streaming app that aggregates content from varies well known music publishers in Ethiopia. Not available on Google play but working other distribution methods"
+                                projectImage={Azemari}
+                                projectLink="https://play.google.com/store/apps/details?id=app.kuwas.android"
+                                projectPlacement="Google Play"
+                            />
+
+                            <ProjectItem
                                 projectName="Hulu Radio"
                                 projectDescription="A radio app designed for Ethiopian people. Contains mostly stations from local broadcasters"
-                                projectImage={Kuwas}
+                                projectImage={Hulu}
                                 projectLink="https://play.google.com/store/apps/details?id=app.huluradio.android"
                                 projectPlacement="Google Play"
                             />
 
-                            <ProjectItem
-                                projectName="Azemari"
-                                projectDescription="A music streaming app that aggregates content from varies well known music publishers in Ethiopia. Not available on Google play but working other distribution methods"
-                                projectImage={Kuwas}
-                                projectLink="https://play.google.com/store/apps/details?id=app.kuwas.android"
-                                projectPlacement="Google Play"
-                            />
 
                             <ProjectItem
-                                projectName="Kuwas"
-                                projectDescription="A sport app designed for Ethiopian premier league. Currently available in the google playstore."
-                                projectImage={Kuwas}
-                                projectLink="https://play.google.com/store/apps/details?id=app.kuwas.android"
+                                projectName="Flick"
+                                projectDescription="Lead dev on an app designed for providing cinema schedule for foreign movies in around Addis Abeba, Ethiopia."
+                                projectImage={Flick}
+                                projectLink="https://play.google.com/store/apps/details?id=com.mnettechnologies.flick"
                                 projectPlacement="Google Play"
                             />
 
