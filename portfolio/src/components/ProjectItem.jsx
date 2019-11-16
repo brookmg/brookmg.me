199d677e0b1a240ed6f0a8d59d8527463af2d506
brookmezgu <brookmezgebu64@gmail.com>
2019-11-16 12:17:15 +0300

    progress: change the picture of kuwas and add more project contents

--- portfolio/src/components/ProjectItem.jsx
+++ portfolio/src/components/ProjectItem.jsx
@@ -18,7 +18,7 @@ class ProjectItem extends Component {
             <div className="column has-text-centered is-one-quarter">
                 <h2> { this.state.projectName } </h2>
 
-                <img className="screenshot" src={ this.state.projectImage} width="257" height="528"/>
+                <img className="screenshot" src={ this.state.projectImage} width="200" height="500"/>
                 <h3 className="has-text-black"><br/> { this.state.projectDescription } <br/><br/>
                     <a href={this.state.projectLink}> { this.state.projectPlacement } </a>
                 </h3>
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import ProjectItem from './ProjectItem.jsx'
-import NewsIcon from '../img/news.png'
+import Kuwas from '../img/Kuwas.jpg'
 
 class Projects extends Component {
 
@@ -15,7 +15,23 @@ class Projects extends Component {
                             <ProjectItem
                                 projectName="Kuwas"
                                 projectDescription="A sport app designed for Ethiopian premier league. Currently available in the google playstore."
-                                projectImage={NewsIcon}
+                                projectImage={Kuwas}
+                                projectLink="https://play.google.com/store/apps/details?id=app.kuwas.android"
+                                projectPlacement="Google Play"
+                            />
+
+                            <ProjectItem
+                                projectName="Hulu Radio"
+                                projectDescription="A radio app designed for Ethiopian people. Contains mostly stations from local broadcasters"
+                                projectImage={Kuwas}
+                                projectLink="https://play.google.com/store/apps/details?id=app.huluradio.android"
+                                projectPlacement="Google Play"
+                            />
+
+                            <ProjectItem
+                                projectName="Azemari"
+                                projectDescription="A music streaming app that aggregates content from varies well known music publishers in Ethiopia. Not available on Google play but working other distribution methods"
+                                projectImage={Kuwas}
                                 projectLink="https://play.google.com/store/apps/details?id=app.kuwas.android"
                                 projectPlacement="Google Play"
                             />
@@ -23,23 +39,7 @@ class Projects extends Component {
                             <ProjectItem
                                 projectName="Kuwas"
                                 projectDescription="A sport app designed for Ethiopian premier league. Currently available in the google playstore."
-                                projectImage={NewsIcon}
-                                projectLink="https://play.google.com/store/apps/details?id=app.kuwas.android"
-                                projectPlacement="Google Play"
-                            />
-
-                            <ProjectItem
-                                projectName="Kuwas"
-                                projectDescription="A sport app designed for Ethiopian premier league. Currently available in the google playstore."
-                                projectImage={NewsIcon}
-                                projectLink="https://play.google.com/store/apps/details?id=app.kuwas.android"
-                                projectPlacement="Google Play"
-                            />
-
-                            <ProjectItem
-                                projectName="Kuwas"
-                                projectDescription="A sport app designed for Ethiopian premier league. Currently available in the google playstore."
-                                projectImage={NewsIcon}
+                                projectImage={Kuwas}
                                 projectLink="https://play.google.com/store/apps/details?id=app.kuwas.android"
                                 projectPlacement="Google Play"
                             />
