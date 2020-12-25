d5b199d52e72f9c94a0c8dbc19707337629478bc
Brook Mezgebu <brookmezgebu64@gmail.com>
2020-12-25 03:04:39 +0300

    ui: add an illustration to fill blank space

--- portfolio/src/components/HeroBody.jsx
+++ portfolio/src/components/HeroBody.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGooglePlay, faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import Img from 'react-cool-img'
+import VCIllustration from "../img/vc_illustration.svg"
 
 class HeroBody extends Component {
 
@@ -9,37 +11,45 @@ class HeroBody extends Component {
             <div className="hero-body is-bold">
 
                 <div className="container introduction level">
-                    <div className="has-text-left is-pulled-left" style={{ width: '50%'}}>
-                        <h1 className="has-text-black is-size-3">Hello, </h1>
+                    <div className="has-text-left is-pulled-left" style={{ width: '30%', flexDirection: 'column', display: 'flex' }}>
+                        
                         <h1 className="has-text-white is-size-1">
-                            <span style={{backgroundColor: `#0bA292`, align: `center`, padding: `8px`}}> Brook </span>
+                            <span style={{backgroundColor: `#0bA292`, align: `center`, padding: `8px`}}><h1 className="has-text-black is-size-1">Hello, </h1> <span style={{ fontWeight: 900 }}> Brook </span> <h1 className="has-text-black is-size-1">Here</h1> </span>
                         </h1>
-                        <h1 className="has-text-black is-size-3">Here</h1>
-
+                        
                         <div className="social">
 
                             <span style={{fontSize: `1.75rem`, padding: `1%`}}>
-                              <a href="https://play.google.com/store/apps/dev?id=8458169143809274686" target="blank">
-                                  <FontAwesomeIcon icon={faGooglePlay} />
-                              </a>
+                                <a href="https://play.google.com/store/apps/dev?id=8458169143809274686" target="blank">
+                                    <FontAwesomeIcon icon={faGooglePlay} />
+                                </a>
                             </span>
 
                             <span style={{fontSize: `1.75rem`, padding: `1%`}}>
-                              <a href="https://github.com/brookmg" target="blank"><FontAwesomeIcon icon={faGithub} /></a>
+                                <a href="https://github.com/brookmg" target="blank"><FontAwesomeIcon icon={faGithub} /></a>
                             </span>
 
                             <span style={{fontSize: `1.75rem`, padding: `1%`}}>
-                              <a href="https://twitter.com/brook_mezgebu" target="blank"><FontAwesomeIcon icon={faTwitter} /></a>
+                                <a href="https://twitter.com/brook_mezgebu" target="blank"><FontAwesomeIcon icon={faTwitter} /></a>
                             </span>
 
                             <span style={{fontSize: `1.75rem`, padding: `1%`}}>
-                              <a href="https://www.linkedin.com/in/brook-mezgebu/" target="blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+                                <a href="https://www.linkedin.com/in/brook-mezgebu/" target="blank"><FontAwesomeIcon icon={faLinkedin} /></a>
                             </span>
-
                         </div>
-
                     </div>
+
+                    <Img
+                            style={{ flex: 'auto' }}
+                            src={ VCIllustration }
+                            alt={`Illustration of moi?`}
+                            debounce={ 1000 }
+                            lazy
+                        />
+
                 </div>
+
+
 
             </div>
         );
